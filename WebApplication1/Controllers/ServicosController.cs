42341f58d5c1adda4391b9e699a84d2726b49166
gabriel <gabriel.jmaciel13@gmail.com>
2025-06-10 06:30:03 -0300

    dropdown de serviços feito.

---
+++ WebApplication1/Controllers/ServicosController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class ServicosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ConsulteDebito()
+        {
+            return View();
+        }
+        public IActionResult EmitirSegundaVia()
+        {
+            return View();
+        }
+        public IActionResult AnexarDocumentos()
+        {
+            return View();
+        }
+    }
+}
