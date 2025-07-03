beb9ca87b1b19e10331fdc909c86c1e2642e1198
Gabriel Borba <borba@gmail.com>
2025-07-03 08:07:54 -0300

    Criando o banco dnv

--- WebApplication1/AppDbContextFactory.cs
+++ WebApplication1/AppDbContextFactory.cs
@@ -1,6 +1,20 @@
-﻿namespace WebApplication1
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
 {
-    public class AppDbContextFactory
+    public AppDbContext CreateDbContext(string[] args)
     {
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+        optionsBuilder.UseSqlite(configuration.GetConnectionString("DefaultConnection"));
+
+        return new AppDbContext(optionsBuilder.Options);
     }
 }
